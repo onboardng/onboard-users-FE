0c40eb2fa455b10e271473f1f70e351ed5bb1489
Phenom <tijjken@gmail.com>
2022-10-13 12:34:38 +0100

    added loader

--- src/components/Loader/PageLoader.tsx
+++ src/components/Loader/PageLoader.tsx
@@ -2,17 +2,19 @@ import "./style.css";
 
 const PageLoader = () => {
   return (
-    <div className="wave-center">
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
-      <div className="wave"></div>
+    <div className="fixed w-screen h-screen top-0 left-0 bg-[rgba(255,255,255,0.8)] z-[55] flex items-center justify-center">
+      <div className="wave-center">
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+      </div>
     </div>
   );
 };
