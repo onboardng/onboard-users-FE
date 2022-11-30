05bed6474d279f7959e4f09f1bebb20ffb1797ee
Samson Okunola <smsnmicheal@gmail.com>
2022-11-30 08:34:30 +0100

    disabled the input fields for school and country in search

--- src/components/school/SearchFields.tsx
+++ src/components/school/SearchFields.tsx
@@ -71,6 +71,7 @@ const SearchFields = ({ setEdit }: { setEdit?: Function }) => {
             onFocus={() => setShow(true)}
             handleChange={formik.handleChange}
             value={formik.values.school_name}
+            disabled
             name="school_name"
             label="School name"
             iconId="school-icon"
@@ -96,13 +97,13 @@ const SearchFields = ({ setEdit }: { setEdit?: Function }) => {
             </div>
           )}
         </div>
-        {/* <LargeInputBox handleChange={formik.handleChange} value={formik.values.country_name} name="country_name" label="Country" iconId="location-icon" placeholder="Enter Country" /> */}
         <div className="bg-grey-600 text-grey-500 rounded-xl flex gap-4 items-center px-2 md:px-8 py-6 h-full largeselect tab:h-[105px]">
           <Icon width={24} height={24} id="location-icon" />
           <div className="flex flex-col gap-3 w-full">
             <h2 className="text-[14px] md:text-[16px]">Country</h2>
             <LargeSelectBox
               value={formik.values.country_name}
+              disabled
               matches={matches}
               name="country_name"
               placeholder="Enter Country"
@@ -141,27 +142,6 @@ const SearchFields = ({ setEdit }: { setEdit?: Function }) => {
             />
           </div>
         </div>
-        {/* <div className="bg-grey-600 text-grey-500 pr-10 flex gap-4 rounded-xl md:rounded-none md:rounded-tr-xl md:rounded-br-xl items-center px-2 md:px-8 py-6 largeselect tab:h-[105px]">
-          <Icon width={24} height={24} id="course-icon" />
-          <div className="flex flex-col gap-3 w-full">
-            <h2 className="text-[14px] md:text-[16px]">Course</h2>
-            <LargeSelectBox matches={matches} name="course" placeholder="Select Course" handleChange={()=>{}} options={countryCodes} />
-          </div>
-        </div> */}
-        {/* <div className="bg-grey-600 text-grey-500 rounded-xl  md:rounded-tl-xl md:rounded-none  md:rounded-bl-xl flex gap-4 items-center px-8 py-6">
-          <Icon width={24} height={24} id="graduation-icon" />
-          <div className="flex flex-col gap-3">
-            <h2 className="text-[14px] md:text-[16px]">Program</h2>
-            <input type="text" placeholder="Select Country" className="text-[#1B1B1B] outline-none text-[16px] md:text-[20px] bg-transparent" />
-          </div>
-        </div>{" "}
-        <div className="bg-grey-600 text-grey-500 pr-10 flex gap-4 rounded-xl md:rounded-none md:rounded-tr-xl md:rounded-br-xl items-center px-8 py-6">
-          <Icon width={24} height={24} id="course-icon" />
-          <div className="flex flex-col gap-3">
-            <h2 className="text-[14px] md:text-[16px]">Course</h2>
-            <input type="text" placeholder="Select Course" className="text-[#1B1B1B] outline-none text-[16px] md:text-[20px] bg-transparent" />
-          </div>
-        </div> */}
       </div>
       <button
         type="button"
