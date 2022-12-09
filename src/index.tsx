6f240e46084a9179918fa24813c7a78ef6e0fb6d
Samson Okunola <smsnmicheal@gmail.com>
2022-12-09 03:37:11 +0100

    google login

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import { GoogleOAuthProvider } from '@react-oauth/google';
 import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+
 import { store } from "./redux/store";
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
