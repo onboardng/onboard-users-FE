299f132ffe2a92885d625e4b48f1bd1c2a96856a
Samson Okunola <smsnmicheal@gmail.com>
2023-01-18 11:35:00 +0100

    updates

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders home page', () => {
+  render(<App />);
+  const buttonElement = screen.getByText(/search/i);
+  expect(buttonElement).toBeInTheDocument();
+})
